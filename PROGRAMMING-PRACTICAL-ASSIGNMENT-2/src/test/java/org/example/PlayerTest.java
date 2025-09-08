032d901ae934353e1bddf9ceab92ebf7bbafdcf3
ST10466527 <ST10466527@rcconnect.edu.za>
2025-09-08 18:03:59 +0200

    PlayerTest.java

--- PROGRAMMING-PRACTICAL-ASSIGNMENT-2/src/test/java/org/example/PlayerTest.java
+++ PROGRAMMING-PRACTICAL-ASSIGNMENT-2/src/test/java/org/example/PlayerTest.java
@@ -6,14 +6,15 @@ import static org.junit.Assert.*;
 public class PlayerTest {
     @Test
     public void testScoreUpdate() {
-        Player p = new Player("Silindokuhlle");
+        Player p = new Player("Silindokuhle");
         p.addScore(2);
         assertEquals(2, p.getScore());
     }
 
     @Test
     public void testPlayerName() {
-        Player p = new Player("Silindokkuhle");
+        Player p = new Player("Silindokuhle");
         assertEquals("Silindokuhle", p.getName());
     }
 }
+
